c9103342cab82f0b7bf9fa49c3b3b9c103bce011
BeniaminBuzun <benekbuz@gmail.com>
2025-04-22 22:09:53 +0200

    relations

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    sensors = db.relationship('Sensor', backref='owner', lazy=True)
+
     def setPassword(self, password):
         self.password = generate_password_hash(password)
 
@@ -23,9 +25,11 @@ class User(UserMixin,db.Model):
 # sensors database
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ownerId = db.Column(db.Integer,nullable=False)
+    ownerId = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
     roomId = db.Column(db.Integer, nullable=False)
     unit = db.Column(db.String(50), nullable=False)
+    sensors = db.relationship('Measurement', backref='owner', lazy=True)
+
     def __repr__(self):
         return f"<Sensor {self.id} - OwnerId:{self.ownerId}>"
 
@@ -33,10 +37,11 @@ class Sensor(db.Model):
 # sensor reading database
 class Measurement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sensorId = db.Column(db.Integer,nullable=False)
+    sensorId = db.Column(db.Integer,db.ForeignKey('sensor.id'),nullable=False)
     measurementTime = db.Column(db.Time,nullable=False)
     measurementDate = db.Column(db.Date,nullable=False)
     measurementValue = db.Column(db.Integer,nullable=True)
+
     def __repr__(self):
         return f"<Sensor {self.sensorId} - Value:{self.measurementValue}>"
 
